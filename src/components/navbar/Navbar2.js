8679a401741db2b74e170be5e7ecee8b813887ac
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-09-03 13:41:26 +0530

    Update Navbar2.js

--- src/components/navbar/Navbar2.js
+++ src/components/navbar/Navbar2.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
-// import "./Navbar2.css";
+import "../Navbar/Navbar2.css"
 
 const Navbar2 = () =>{
     return (
@@ -11,9 +11,8 @@ const Navbar2 = () =>{
             <Container>
               <Navbar.Brand href="#home"> 📧 Mail Box Client</Navbar.Brand>
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <NavLink to='/signup' className='bar'>SignUp</NavLink>
+                <NavLink to='/login' className='bar'>Login</NavLink>
               </Nav>
             </Container>
           </Navbar>
